f1d5fcc7b14df501a299fff88c392ec583bc07a9
Ludovic Druette <ludovicdruette@protonmail.com>
2023-03-11 16:29:23 +0100

    Standardize arguments notation for temporal scheme functions

--- numerical_scheme.py
+++ numerical_scheme.py
@@ -3,22 +3,22 @@ import numpy as np
 import values
 
 
-def simplectic_scheme(theta0, thetadot0, omega0, dt, n):
+def simplectic_scheme(theta0, theta_dot0, omega0, dt, N):
     """
     Compute the value for theta and theta_dot for N iteration of duratation dt
     with an simplectic temporal scheme
     """
 
-    theta = np.zeros(n + 1)
-    thetadot = np.zeros(n + 1)
+    theta = np.zeros(N + 1)
+    theta_dot = np.zeros(N + 1)
     theta[0] = theta0
-    thetadot[0] = thetadot0
+    theta_dot[0] = theta_dot0
 
-    for i in range(n - 1):
-        thetadot[i + 1] = thetadot[i] - omega0**2 * theta[i] * dt
-        theta[i + 1] = theta[i] + thetadot[i + 1] * dt
+    for i in range(N - 1):
+        theta_dot[i + 1] = theta_dot[i] - omega0**2 * theta[i] * dt
+        theta[i + 1] = theta[i] + theta_dot[i + 1] * dt
 
-    return theta, thetadot
+    return theta, theta_dot
 
 
 def explicit_scheme(theta0, theta_dot0, omega0, dt, N):
